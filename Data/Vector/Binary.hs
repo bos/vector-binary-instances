ac392014ed657a5456a52c05a2dc33b8ba3fa7a6
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2013-04-16 04:13:04 +0400

    Expose generic {de,}serialization functions
    They are also renamed to match names from cereal-vector.
    Longer names are less ambigious too.

--- Data/Vector/Binary.hs
+++ Data/Vector/Binary.hs
@@ -15,6 +15,13 @@
 -- making it easy to serialize vectors to and from disk. We use the
 -- generic interface to vectors, so all vector types are supported.
 --
+-- All functions in this module use same data format. Different
+-- representations for vector length and its elements could be used
+-- but general shape is same.
+--
+-- > [number of elements]
+-- > [vector element    ] : N times
+--
 -- To serialize a vector:
 --
 -- > *Data.Vector.Binary> let v = Data.Vector.fromList [1..10]
@@ -29,8 +36,12 @@
 -- > Chunk "\US\139\b\NUL\NUL\N...\229\240,\254:\NUL\NUL\NUL" Empty
 --
 --------------------------------------------------------------------
-
-module Data.Vector.Binary () where
+module Data.Vector.Binary (
+    genericGetVector
+  , genericGetVectorWith
+  , genericPutVector
+  , genericPutVectorWith
+  ) where
 
 import Data.Binary
 
@@ -46,39 +57,57 @@ import Foreign.Storable (Storable)
 
 -- | Boxed, generic vectors.
 instance Binary a => Binary (Vector a) where
-    put = putGeneric
-    get = getGeneric
+    put = genericPutVector
+    get = genericGetVector
     {-# INLINE get #-}
 
 -- | Unboxed vectors
 instance (U.Unbox a, Binary a) => Binary (U.Vector a) where
-    put = putGeneric
-    get = getGeneric
+    put = genericPutVector
+    get = genericGetVector
     {-# INLINE get #-}
 
 -- | Primitive vectors
 instance (P.Prim a, Binary a) => Binary (P.Vector a) where
-    put = putGeneric
-    get = getGeneric
+    put = genericPutVector
+    get = genericGetVector
     {-# INLINE get #-}
 
 -- | Storable vectors
 instance (Storable a, Binary a) => Binary (S.Vector a) where
-    put = putGeneric
-    get = getGeneric
+    put = genericPutVector
+    get = genericGetVector
     {-# INLINE get #-}
 
 ------------------------------------------------------------------------
 
-getGeneric :: (G.Vector v a, Binary a) => Get (v a)
-{-# INLINE getGeneric #-}
-getGeneric = do
-    n  <- get
-    G.replicateM n get
+-- | Deserialize vector using custom parsers.
+genericGetVectorWith :: (G.Vector v a, Binary a)
+    => Get Int       -- ^ Parser for vector size
+    -> Get a         -- ^ Parser for vector's element
+    -> Get (v a)
+{-# INLINE genericGetVectorWith #-}
+genericGetVectorWith getN getA = do
+    n  <- getN
+    G.replicateM n getA
+
+-- | Generic put for anything in the G.Vector class which uses custom
+--   encoders.
+genericPutVectorWith :: (G.Vector v a, Binary a)
+    => (Int -> Put)  -- ^ Encoder for vector size
+    -> (a   -> Put)  -- ^ Encoder for vector's element
+    -> v a -> Put
+{-# INLINE genericPutVectorWith #-}
+genericPutVectorWith putN putA v = do
+    putN (G.length v)
+    G.mapM_ putA v
+
+-- | Generic function for vector deserialization.
+genericGetVector :: (G.Vector v a, Binary a) => Get (v a)
+{-# INLINE genericGetVector #-}
+genericGetVector = genericGetVectorWith get get
 
 -- | Generic put for anything in the G.Vector class.
-putGeneric :: (G.Vector v a, Binary a) => v a -> Put
-{-# INLINE putGeneric #-}
-putGeneric v = do
-    put (G.length v)
-    G.mapM_ put v
+genericPutVector :: (G.Vector v a, Binary a) => v a -> Put
+{-# INLINE genericPutVector #-}
+genericPutVector = genericPutVectorWith put put
